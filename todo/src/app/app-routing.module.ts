021e2e4512e04e8e0aff6ee6c8f94bb9637b8e70
nichelia <nich.elia@gmail.com>
2020-09-04 19:07:53 +0300

    Route to home only if registered admin

--- todo/src/app/app-routing.module.ts
+++ todo/src/app/app-routing.module.ts
@@ -1,17 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { redirectUnauthorizedTo, redirectLoggedInTo, canActivate } from '@angular/fire/auth-guard';
+import { hasCustomClaim, redirectUnauthorizedTo, redirectLoggedInTo, canActivate } from '@angular/fire/auth-guard';
 import { unauthorisedRoute, authorisedRoute } from '../environments/environment';
 
 import { HomeComponent } from './home/home.component';
 import { AuthComponent } from './auth/auth.component';
 
+const adminOnly = () => hasCustomClaim('admin');
 const redirectUnauthorizedToAuth = () => redirectUnauthorizedTo([unauthorisedRoute]);
 const redirectLoggedInToHome = () => redirectLoggedInTo([authorisedRoute]);
 
 const routes: Routes = [
   { path: 'auth', component: AuthComponent, ...canActivate(redirectLoggedInToHome) },
-  { path: 'home', component: HomeComponent, ...canActivate(redirectUnauthorizedToAuth) },
+  { path: 'home', component: HomeComponent, ...canActivate(adminOnly) },
   { path: '', redirectTo: 'auth', pathMatch: 'full' },
   { path: '**', redirectTo: 'auth', pathMatch: 'full' },
 ];
